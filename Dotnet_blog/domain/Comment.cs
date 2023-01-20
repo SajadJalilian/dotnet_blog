2e3441f7002983c4701e5d6c015b4e6b543688c6
Sajad Jalilian <sajadjalilian88@gmail.com>
2023-01-20 23:21:39 +0330

    Add some more props to Comment and Post

--- Dotnet_blog/domain/Comment.cs
+++ Dotnet_blog/domain/Comment.cs
@@ -9,6 +9,7 @@ public sealed class Comment : BaseEntity
 {
     public string Name { get; set; }
     public string Content { get; set; }
+    public string Email { get; set; }
 
     // Navigation
     public Post Post { get; set; }
--- Dotnet_blog/domain/Post.cs
+++ Dotnet_blog/domain/Post.cs
@@ -9,4 +9,8 @@ public sealed class Post : BaseEntity
 {
     public string Title { get; set; }
     public string Content { get; set; }
+
+    // Navigation
+    public User Author { get; set; }
+    public IEnumerable<Category> Categories { get; set; }
 }
